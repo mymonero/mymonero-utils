61f66d0b8aa052563ae2ff7021b01f27c11f015a
Karl Buys <kbbuys@gmail.com>
2021-05-17 15:13:58 +0200

    Added currency metadata (name, ticker code)

---
+++ packages/mymonero-exchange-helper/CurrencyMetadata.js
@@ -0,0 +1,20 @@
+currencyMetadata = {
+    "ETH": {
+        fullName: "Ethereum",
+        precision: 18
+    },
+    "LTC": {
+        fullName: "Litecoin",
+        precision: 8
+    },
+    "BTC": {
+        fullName: "Bitcoin",
+        precision: 8
+    },
+    "XMR": {
+        fullName: "Monero",
+        precision: 12
+    }
+}
+
+module.exports = currencyMetadata
