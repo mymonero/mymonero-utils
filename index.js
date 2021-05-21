34c9aafc7d729fc806f1c97575241f5591168570
Karl Buys <kbbuys@gmail.com>
2021-05-21 12:04:23 +0200

    Examples for invoking methods updated

--- index.js
+++ index.js
@@ -67,25 +67,77 @@ async function createOrder() {
 async function getEstimatedAmount() {
     let instance = new integration();
     let response
+
     let fromCurrency = 'xmr';
     let toCurrency = 'btc';
     let flow = "standard";
     let fromAmount = "0.5";
     let toAmount = "";
+    // try {
+    //     response = await instance.getEstimatedAmount(fromCurrency, toCurrency, flow, fromAmount, toAmount);
+    // } catch (error) {
+    //     console.log(error);
+    // }
+    // console.log(response);
+    //https://api.changenow.io/v2/exchange/estimated-amount?fromCurrency=btc&toCurrency=usdt&fromAmount=0.1&toAmount=&fromNetwork=btc&toNetwork=eth&flow=fixed-rate&type=&useRateId=true
+
+    // fromCurrency = "btc";
+    // toCurrency = "usdt";
+    // fromAmount = "0.1";
+    // toAmount = "";
+    // flow = "fixed-rate";
+    
     try {
         response = await instance.getEstimatedAmount(fromCurrency, toCurrency, flow, fromAmount, toAmount);
     } catch (error) {
         console.log(error);
     }
-    console.log(response);
+    console.log(response)
+}
+
+async function createOrder() {
+    let instance = new integration();
+    let response
+
+    let fromCurrency = "xmr"
+    let toCurrency = "btc"
+    let flow = "standard"
+    let fromAmount = "0.5"
+    let toAmount = ""
+    let type = "direct"
+    let refundAddress = "47pasa5moXNCSyvvip6sY39VFGYymMhVEXpcaZSaP3hAVNbVXpGu5MVZn9ePeotMRFiJuLq2pB6B3Hm7uWYanyJe1yeSbm9"
+    let destinationAddress = "3E6iM3nAY2sAyTqx5gF6nnCvqAUtMyRGEm";
+    try {
+        // createTransaction(fromCurrency, toCurrency, flow, fromAmount, toAmount, refundAddress, type = "direct") {
+        response = await instance.createTransaction(fromCurrency, toCurrency, flow, fromAmount, toAmount, destinationAddress, refundAddress, type);
+        
+    } catch (error) {
+        console.log(error);
+    }
+    console.log(response)
+}
+
+async function getTransactionStatus() {
+    let instance = new integration();
+    let response
+
+    let id = "ca20f5056d502b";
+    try {
+        // createTransaction(fromCurrency, toCurrency, flow, fromAmount, toAmount, refundAddress, type = "direct") {
+        response = await instance.getTransactionStatus(id)
+        
+    } catch (error) {
+        console.log(error);
+    }
+    console.log(response)
 }
 
 (async() => {
 
     console.log('before start');
     let response;
-    response = await initialise();
-    console.log(response);
+    // response = await initialise();
+    // console.log(response);
 
     // response = await getMinimal();
     // console.log(response);
@@ -95,7 +147,14 @@ async function getEstimatedAmount() {
 
     // response = await createOrder()
     // console.log(response);
-    response = await getEstimatedAmount();
+    
+    // response = await getEstimatedAmount();
+    // console.log(response);
+
+    // response = await createOrder();
+    // console.log(response);
+
+    response = await getTransactionStatus();
     console.log(response);
 
     console.log('after start');
