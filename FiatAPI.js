47ea27541e6d8ca6d52829d07092cbc69663f578
Karl Buys <kbbuys@gmail.com>
2021-09-09 11:53:58 +0200

    style: cleaned up debug output

--- FiatAPI.js
+++ FiatAPI.js
@@ -1,111 +1,16 @@
+// This file facilitates integration with the ChangeNow Fiat API
+// Up-to-date documentation can be found here: https://documenter.getpostman.com/view/8180765/SVfTPnM8?version=latest
+
 const axios = require("axios");
 const apiKey = require("./apiKey");
-
-
-/**
- * 
- * Fiat operations
- * POST - https://api.changenow.io/v2/fiat-transaction
- * 
-POST
-Create exchange transaction with fiat
-https://api.changenow.io/v2/fiat-transaction
-BODY PARAMETERS:
-  "from_amount": 1200.24,
-  "from_currency": "EUR",
-  "to_currency": "BTC",
-  "from_network": null,
-  "to_network": "BNB",
-  "payout_address": "mtXWDB6k5yC5v7TcwKZHB89SUp85yCKshy",
-  "payout_extra_id": "1",
-  "deposit_type": "SEPA_1",
-  "payout_type": "SEPA_1",
-  "external_partner_link_id": ""
-
-
-RESPONSE PARAMETERS: 
-{
-  "id": "4496229738",
-  "status": "new",
-  "email": null,
-  "errors": [],
-  "status_details": null,
-  "from_currency": "EUR",
-  "from_network": null,
-  "from_currency_with_network": null,
-  "from_amount": "0",
-  "deposit_type": "SEPA_1",
-  "payout_type": "CRYPTO_THROUGH_CN",
-  "expected_from_amount": "1200.24",
-  "to_currency": "TRX",
-  "to_network": null,
-  "to_currency_with_network": null,
-  "to_amount": null,
-  "output_hash": null,
-  "expected_to_amount": "23445.66041",
-  "location": "DE",
-  "created_at": "2021-07-14T10:36:40.273Z",
-  "updated_at": "2021-07-14T10:36:40.273Z",
-  "partner_id": "6437862205",
-  "external_partner_link_id": null,
-  "estimate_breakdown": {
-    "toAmount": "23445.66041",
-    "fromAmount": 1200.24,
-    "serviceFees": [
-      {
-        "name": "Service fee",
-        "amount": "2.49",
-        "currency": "EUR"
-      }
-    ],
-    "convertedAmount": {
-      "amount": "1197.75",
-      "currency": "EUR"
-    },
-    "estimatedExchangeRate": "19.57475300",
-    "networkFee": {
-      "amount": "0.1",
-      "currency": "TRX"
-    }
-  },
-  "payout": {
-    "address": "0x7cC3BD073c6d9564bb67ffCb86f76D36e48ce3F1",
-    "extra_id": "1"
-  },
-  "redirect_url": "https://payments.guardarian.com/checkout?tid=4496229738"
-}
-GET
-Transaction status
-https://api.changenow.io/v2//fiat-transaction/?id=id
-GET
-Estimate
-https://api.changenow.io/v2/fiat-estimate?from_currency=&from_network=&from_amount=&to_currency=&to_network=&deposit_type=&payout_type=
-GET
-MarketInfo
-https://api.changenow.io/v2/fiat-market-info/min-max-range/{fromto}
-GET
-Fiat helthcheck service
-https://api.changenow.io/v2/fiat-status
-GET
-Fiat currencies
-https://api.changenow.io/v2/fiat-currencies/fiat
-GET
-Crypto currencies
-https://api.changenow.io/v2/fiat-currencies/crypto
- */
-
-
-
 class FiatAPI {
     constructor(settings) {
         this.apiUrl = "https://api.changenow.io";
         this.apiVersion = "v2";
-        // this.currencyToExchange = "xmr2btc";
         this.apiKey = settings.apiKey;
     }
 
     getApiPath() {
-        console.log(this);
         return `${this.apiUrl}/${this.apiVersion}/`
     }
 
@@ -278,7 +183,7 @@ class FiatAPI {
             };
 
             axios(config).then(function (response) {
-                console.log(JSON.stringify(response.data));
+
                 resolve(response.data);
             }).catch((error) => {
                 reject(error);
