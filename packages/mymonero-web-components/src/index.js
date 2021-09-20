91218185447114b97a66bff669acd6d91d08fa68
Karl Buys <kbbuys@gmail.com>
2021-09-20 12:06:49 +0200

    refactor: more tweaks to enable transpilation

---
+++ packages/mymonero-web-components/src/index.js
@@ -0,0 +1,11 @@
+/* Require reusable components */
+require("../lib/Components/Shared/ActivityIndicator");
+require("../lib/Components/Shared/ProviderCard");
+require("../lib/Components/Shared/SearchableSelect")
+require("../lib/Components/Shared/WalletSelector");
+
+/* Import page templates */
+// import ExchangeLandingPage from "./Components/Exchange/ExchangeLandingPage";
+
+/* Export page templates */
+module.exports = { }
