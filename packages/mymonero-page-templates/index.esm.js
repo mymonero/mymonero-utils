1e493ef45ec3e00b5d87f1fefffc22359b17dc5a
Karl Buys <kbbuys@gmail.com>
2021-09-29 13:14:35 +0200

    refactor: change import paths to match new directory structure

--- packages/mymonero-page-templates/index.esm.js
+++ packages/mymonero-page-templates/index.esm.js
@@ -2,13 +2,13 @@
 require("@mymonero/mymonero-web-components");
 
 /* Require various view elements */
-require("./Exchange/Elements/ChangenowBuyWithFiatView");
-require("./Exchange/Elements/ChangenowFixedRateView")
-require("./Exchange/Elements/ChangenowFloatingRateView");
+require("./src/Exchange/Elements/ChangenowBuyWithFiatView");
+require("./src/Exchange/Elements/ChangenowFixedRateView")
+require("./src/Exchange/Elements/ChangenowFloatingRateView");
 
 /* Import exchange landing page class */
-import ExchangeLandingPage from "./Exchange/Elements/ExchangeLandingPage";
-import ExchangeNavigationController from "./Exchange/Controllers/ExchangeNavigationController";
+import ExchangeLandingPage from "./src/Exchange/Elements/ExchangeLandingPage";
+import ExchangeNavigationController from "./src/Exchange/Controllers/ExchangeNavigationController";
 
 /* Export page templates */
 export default { 
