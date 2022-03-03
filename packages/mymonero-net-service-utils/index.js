93de1b612758c610f567223c8b6463142539bdbd
Devin <devin@blackhat.co.za>
2022-03-03 15:03:14 +0200

    Updated to r

--- packages/mymonero-net-service-utils/index.js
+++ packages/mymonero-net-service-utils/index.js
@@ -1,7 +1,5 @@
 'use strict'
 
-const xhr = require('xhr')
-
 function New_ParametersForWalletRequest (address, view_key__private) {
   return {
     address: address,
@@ -17,32 +15,36 @@ function AddUserAgentParamters (parameters, appUserAgent_product, appUserAgent_v
 }
 exports.AddUserAgentParamters = AddUserAgentParamters
 
-function HTTPRequest (apiAddressAuthority, endpointPath, finalParameters, fn) {
-  if (typeof finalParameters === 'undefined' || finalParameters == null) {
-    throw Error('final_parameters must not be nil')
+function HTTPRequest (
+  request_conformant_module, // such as 'request' or 'xhr' .. TODO: consider switching to 'fetch'
+  apiAddress_authority, // authority means [subdomain.]host.…[:…] with no trailing slash
+  endpointPath,
+  final_parameters,
+  fn
+) {
+  // fn: (err?, data?) -> new Request
+  if (typeof final_parameters === 'undefined' || final_parameters == null) {
+    throw 'final_parameters must not be nil'
+    // return null
   }
   const completeURL = apiAddress_authority + endpointPath
   console.log('📡  ' + completeURL)
-
-  const requestOptions = {
-    method: 'POST',
-    url: completeURL,
-    headers: {
-      'Content-Type': 'application/json',
-      Accept: 'application/json'
-    },
-    json: finalParameters,
-    useXDR: true, // CORS
-    withCredentials: true // CORS
-  }
-
-  const requestHandle = xhr(
-    requestOptions,
+  //
+  const request_options = _new_requestOptions_base(
+    'POST',
+    completeURL,
+    final_parameters
+  )
+  const requestHandle = request_conformant_module(
+    request_options,
     function (err_orProgressEvent, res, body) {
-      _new_HTTPRequestHandlerFunctionCallingFn(completeURL,
+      _new_HTTPRequestHandlerFunctionCallingFn(fn)(
+        // <- called manually instead of directly passed to request_conformant_module call to enable passing completeURL
+        completeURL,
         err_orProgressEvent,
         res,
-        body, fn)
+        body
+      )
     }
   )
   //
@@ -50,48 +52,70 @@ function HTTPRequest (apiAddressAuthority, endpointPath, finalParameters, fn) {
 }
 exports.HTTPRequest = HTTPRequest
 
-function _new_HTTPRequestHandlerFunctionCallingFn (completeURL, err_orProgressEvent, res, body, fn) {
-  // err appears to actually be a ProgressEvent
-  var err = null
-  const statusCode = typeof res !== 'undefined' ? res.statusCode : -1
-  if (statusCode == 0 || statusCode == -1) {
-    // we'll treat 0 as a lack of internet connection.. unless there's a better way to make use of err_orProgressEvent which is apparently going to be typeof ProgressEvent here
-    err = new Error('Connection Failure')
-  } else if (statusCode !== 200) {
-    const body_Error =
+function _new_APIAddress_baseURLString (
+  apiAddress_authority // authority means [subdomain.]host.…[:…]
+) {
+  return apiAddress_authority
+}
+
+function _new_requestOptions_base (methodName, completeURL, json_parameters) {
+  return {
+    method: methodName,
+    url: completeURL,
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json'
+    },
+    json: json_parameters,
+    useXDR: true, // CORS
+    withCredentials: true // CORS
+  }
+}
+
+function _new_HTTPRequestHandlerFunctionCallingFn (fn) {
+  return function (completeURL, err_orProgressEvent, res, body) {
+    // err appears to actually be a ProgressEvent
+    var err = null
+    const statusCode = typeof res !== 'undefined' ? res.statusCode : -1
+    if (statusCode == 0 || statusCode == -1) {
+      // we'll treat 0 as a lack of internet connection.. unless there's a better way to make use of err_orProgressEvent which is apparently going to be typeof ProgressEvent here
+      err = new Error('Connection Failure')
+    } else if (statusCode !== 200) {
+      const body_Error =
 				body && typeof body === 'object' ? body.Error : undefined
-    const statusMessage =
+      const statusMessage =
 				res && res.statusMessage ? res.statusMessage : undefined
-    if (typeof body_Error !== 'undefined' && body_Error) {
-      err = new Error(body_Error)
-    } else if (typeof statusMessage !== 'undefined' && statusMessage) {
-      err = new Error(statusMessage)
+      if (typeof body_Error !== 'undefined' && body_Error) {
+        err = new Error(body_Error)
+      } else if (typeof statusMessage !== 'undefined' && statusMessage) {
+        err = new Error(statusMessage)
+      } else {
+        err = new Error('Unknown ' + statusCode + ' error')
+      }
+    }
+    if (err) {
+      console.error('❌  ' + err)
+      // console.error("Body:", body)
+      fn(err, null)
+      return
+    }
+    var json
+    if (typeof body === 'string') {
+      try {
+        json = JSON.parse(body)
+      } catch (e) {
+        console.error(
+          '❌  HostedMoneroAPIClient Error: Unable to parse json with exception:',
+          e,
+          '\nbody:',
+          body
+        )
+        fn(e, null)
+      }
     } else {
-      err = new Error('Unknown ' + statusCode + ' error')
+      json = body
     }
+    console.log('✅  ' + completeURL + ' ' + statusCode)
+    fn(null, json)
   }
-  if (err) {
-    console.error('❌  ' + err)
-    // console.error("Body:", body)
-    fn(err, null)
-    return
-  }
-  var json
-  if (typeof body === 'string') {
-    try {
-      json = JSON.parse(body)
-    } catch (e) {
-      console.error(
-        '❌  HostedMoneroAPIClient Error: Unable to parse json with exception:',
-        e,
-        '\nbody:',
-        body
-      )
-      fn(e, null)
-    }
-  } else {
-    json = body
-  }
-  console.log('✅  ' + completeURL + ' ' + statusCode)
-  fn(null, json)
 }
