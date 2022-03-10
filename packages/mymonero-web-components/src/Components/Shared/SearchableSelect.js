38676fb38419561b855e5af6481052fc1fcec1b8
Karl Buys <kbbuys@gmail.com>
2022-03-10 10:10:23 +0200

    fix: for some reason, optional chaining throws an unexpected token error on Android

--- packages/mymonero-web-components/src/Components/Shared/SearchableSelect.js
+++ packages/mymonero-web-components/src/Components/Shared/SearchableSelect.js
@@ -155,7 +155,7 @@ export class SearchableSelect extends LitElement {
     }
 
     willUpdate(changedProperties) {
-        if (changedProperties.get("values")?.length > 0) {
+        if (changedProperties.get("values").length > 0) {
             //this.showDropdown = false;
             this.filteredValues = this.values;
 
