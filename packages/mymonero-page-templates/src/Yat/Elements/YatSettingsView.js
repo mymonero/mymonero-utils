a8a975130818d1140ab12c5eec82a3789c0a9aeb
Karl Buys <kbbuys@gmail.com>
2022-04-05 13:33:28 +0200

    fix: add wallet address to link to global context obj

--- packages/mymonero-page-templates/src/Yat/Elements/YatSettingsView.js
+++ packages/mymonero-page-templates/src/Yat/Elements/YatSettingsView.js
@@ -1,7 +1,6 @@
 import {html, css, LitElement } from 'lit';
 import Swal from 'sweetalert2';
 
-
 export class YatSettingsView extends LitElement {
     static get styles() {
         return css`
@@ -41,14 +40,12 @@ export class YatSettingsView extends LitElement {
     connectedCallback() {
         super.connectedCallback();
         this.wallets = this.context.walletsListController.records;
-        // this.partnerPath = "https://y.at/partner/mymonero"
-        this.partnerPath = "https://yat.fyi/partner/mymonero"
+        this.partnerPath = "https://y.at/partner/mymonero" // Live url
+        // this.partnerPath = "https://yat.fyi/partner/mymonero" // debug url
     }
         
     constructor() {
         super();
-        // this.clickHandler = this.clickHandler;
-        // this.inputRef = createRef();
     }
         
     static get properties() {
@@ -75,11 +72,12 @@ export class YatSettingsView extends LitElement {
         // refresh_token:  User's refresh token that was received in the response body of redirection to Yat web or as a deep link query parameter in redirection from Yat web in Get a Yat (Flow 1A).
         let partnerPath = this.partnerPath;
         
-        // 1. Get refresh token
-        // 2. Create URL
-        // 3. openExternal(url)
         // Uncomment the next line once we have proper Yat management functionality in the core wallet experience
-        // let addressString = `0x1001=${this.context.walletsListController.records[0]}`
+        let addressString = `0x1001=${this.context.walletsListController.records[0]}`
+        partnerPath += `?${addressString}`
+        if (this.context.refresh_token !== undefined) {
+            partnerPath += "&refresh_token=" + this.context.refresh_token
+        }
         this.openExternal(partnerPath);
     }
 
@@ -90,113 +88,76 @@ export class YatSettingsView extends LitElement {
         // addresses: 
         
         if (this.context.walletsListController.records.length > 0) {
-            let refresh_token = "placeholder";
-        let walletsString;
-        let inputObj = {};
-        this.wallets.forEach((wallet) => {
-            console.log(wallet);
-            inputObj[wallet.public_address] = wallet.walletLabel;
-            walletsString += `<option value='${wallet.public_address}'>${wallet.walletLabel}</option>`
-        })
+            let walletsString;
+            let inputObj = {};
+            this.wallets.forEach((wallet) => {
+                console.log(wallet);
+                inputObj[wallet.public_address] = wallet.walletLabel;
+                walletsString += `<option value='${wallet.public_address}'>${wallet.walletLabel}</option>`
+            })
 
-        let html = `
-            <select>
-                ${walletsString}
-            </select>
-        `;
+            let html = `
+                <select>
+                    ${walletsString}
+                </select>
+            `;
 
-        const { value: address } = await Swal.fire({
-            input: 'select',
-            inputOptions: inputObj,
-            inputPlaceholder: 'Select a wallet to link to your Yat handle',
-            showCancelButton: true,
-            title: 'Please select a wallet to connect to',
-            background: "#272527",
-            titleColor: "#FFFFFF",
-            color: "#FFFFFF",
-            confirmButtonColor: "#11bbec",
-            confirmButtonText: 'Connect a Yat',
-            cancelButtonText: 'Close',
-            showCloseButton: true,
-            showCancelButton: true,
-            inputOptions: inputObj,
-            inputPlaceholder: 'Select a wallet',
-            showCancelButton: true,
-            inputValidator: (value) => {
-                console.log(value);
-            return new Promise((resolve) => {
-                console.log(value);
-                if (value !== '') {
-                resolve()
-                } else {
-                resolve('You need to select a wallet')
+            const { value: address } = await Swal.fire({
+                input: 'select',
+                inputOptions: inputObj,
+                inputPlaceholder: 'Select a wallet to link to your Yat handle',
+                showCancelButton: true,
+                title: 'Please select a wallet to connect to',
+                background: "#272527",
+                color: "#FFFFFF",
+                confirmButtonColor: "#11bbec",
+                confirmButtonText: 'Connect a Yat',
+                cancelButtonText: 'Close',
+                showCloseButton: true,
+                showCancelButton: true,
+                inputOptions: inputObj,
+                inputPlaceholder: 'Select a wallet',
+                showCancelButton: true,
+                inputValidator: (value) => {
+                    console.log(value);
+                    return new Promise((resolve) => {
+                        console.log(value);
+                        if (value !== '') {
+                            resolve()
+                        } else {
+                            resolve('You need to select a wallet')
+                        }
+                    })
+                },
+                customClass: {
+                    confirmButton: 'base-button hoverable-cell navigation-blue-button-enabled action right-save-button',
+                    cancelButton: 'base-button hoverable-cell navigation-blue-button-enabled action right-save-button disabled navigation-blue-button-disabled'
                 }
             })
-            },
-            customClass: {
-                confirmButton: 'base-button hoverable-cell navigation-blue-button-enabled action right-save-button',
-                cancelButton: 'base-button hoverable-cell navigation-blue-button-enabled action right-save-button disabled navigation-blue-button-disabled'
-            }
-        })
             
             if (address) {
+                this.context.walletsListController.addressToLink = address;
                 let addressString = encodeURIComponent(`0x1001=${address}`);
-                //let refreshToken = "todolol"
-                //let partnerPath = `https://y.a.bblat/partner/{partner_path}/link-email?addresses=${addressString}&refresh_token=${refreshToken}`
-                let partnerPath = this.partnerPath;
+                let partnerPath = this.partnerPath + "/manage?addresses=" + addressString;
+                if (this.context.refresh_token !== undefined) {
+                    let refresh_token = this.context.refresh_token;
+                    partnerPath += "&refresh_token=" + refresh_token;
+                }
                 this.openExternal(partnerPath);
             }
-            
-            // const { value: wallet_address } = await Swal.fire({
-			// 	title: 'Please select a wallet to connect to',
-			// 	html: html,
-			// 	background: "#272527",
-			// 	titleColor: "#FFFFFF",
-			// 	color: "#FFFFFF",
-			// 	confirmButtonColor: "#11bbec",
-			// 	confirmButtonText: 'Connect a Yat',
-			// 	cancelButtonText: 'Close',
-			// 	showCloseButton: true,
-            //     showCancelButton: true,
-            //     inputOptions: inputObj,
-            //     inputPlaceholder: 'Select a fruit',
-            //     showCancelButton: true,
-            //     inputValidator: (value) => {
-            //         console.log(value);
-            //         return new Promise((resolve) => {
-            //             if (typeof(value) !== "undefined") {
-            //                 resolve()
-            //             } else {
-            //                 resolve('Broken')
-            //             }
-            //         })
-            //     },
-			// 	customClass: {
-			// 		confirmButton: 'base-button hoverable-cell navigation-blue-button-enabled action right-save-button',
-			// 		cancelButton: 'base-button hoverable-cell navigation-blue-button-enabled action right-save-button disabled navigation-blue-button-disabled'
-            //     },
-            // });
-
-            // if (wallet_address) {
-
-            //     console.log(wallet_address);
-            //     Swal.fire(`You selected: ${wallet_address}`)
-            // }
         } else if (this.context.walletsListController.records.length === 1) {
             // immediate redirect
+            this.context.walletsListController.addressToLink = this.wallets.records[0];
             let addressString = `0x1001=${this.wallets.records[0]}`
-            let partnerPath = `https://y.at/partner/{partner_path}/link-email`
+            let partnerPath = this.partnerPath + "/manage?addresses=" + addressString;
+            if (this.context.refresh_token !== undefined) {
+                let refresh_token = this.context.refresh_token;
+                partnerPath += "&refresh_token=" + refresh_token;
+            }
             this.openExternal(partnerPath);    
         } else {
-            // please log into wallet
+            throw new Error("Cannot link a Yat when zero wallets exist");
         }
-    }
-
-    handleManageAYatClickEvent() {
-        // refresh_token:  User's refresh token that was received in the response body of redirection to Yat web or as a deep link query parameter in redirection from Yat web in Get a Yat (Flow 1A).
-        // addresses: YAT_TAG_1=ADDRESS_1|YAT_TAG_2=ADDRESS_2|...|YAT_TAG_N=ADDRESS_N
-        let partnerPath = "https://y.at/partner/{partner_path}/manage"
-        this.openExternal(partnerPath);
     }
 
     render() {
@@ -206,17 +167,14 @@ export class YatSettingsView extends LitElement {
             </div> -->
             <div class="form-field">
                 <span class="field-title">
-                    Yat<mym-tooltip allowHTML="true" tooltipContent="Yat lets you use emojis as your universal username and identity on the internet.">?</mym-tooltip>
+                    Yat
                 </span>
             </div>
             <div>
                 <a id="buy-a-yat" class="clickable-link" @click=${this.handleBuyAYatClickEvent}>Buy a Yat</a>            
             </div>
             <div>
-                <a id="connect-a-yat" class="clickable-link" @click=${this.handleConnectAYatClickEvent}>Connect a Yat</a>
-            </div>
-            <div>
-                <a id="manage-a-yat" class="clickable-link" @click=${this.handleManageAYatClickEvent}>Manage Yats</a>
+                <a id="connect-a-yat" class="clickable-link" @click=${this.handleConnectAYatClickEvent}>Connect / Manage your Yat(s)</a>
             </div>
         `
     }  
