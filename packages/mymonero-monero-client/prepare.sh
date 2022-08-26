810344b610d9fff98b3a762ece1feaa549f96cb5
amilumibe <amulya.sattor@slaz.com>
2022-08-26 13:00:47 -0400

    updated commit hash for mymonero-core-cpp to webassembly-cleanup branch plus changes for hard fork v15

--- packages/mymonero-monero-client/prepare.sh
+++ packages/mymonero-monero-client/prepare.sh
@@ -6,7 +6,7 @@ set -e # Exit on any error
 
 ## These should be audited!
 mymonero_core_cpp_url='https://github.com/mymonero/mymonero-core-cpp'
-mymonero_core_cpp_hash='dadb28f215ec7d6e52cc29ffa881a30542c64bc5'
+mymonero_core_cpp_hash='7269f53ddec9f51f9aba9f61165cd2cf979b49c3'
 monero_core_custom_url='https://github.com/mymonero/monero-core-custom'
 monero_core_custom_hash='d0d4ec2c99a1db96518a98ff47773d76cf729d84'
 
