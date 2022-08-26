c4c3807f55997928da4d70b0b2f27d8c423d14cd
amilumibe <amulya.sattor@slaz.com>
2022-08-25 23:04:20 -0400

    reverting monero-core-custom hash back to latest master branch

--- packages/mymonero-monero-client/prepare.sh
+++ packages/mymonero-monero-client/prepare.sh
@@ -8,7 +8,7 @@ set -e # Exit on any error
 mymonero_core_cpp_url='https://github.com/mymonero/mymonero-core-cpp'
 mymonero_core_cpp_hash='64c37a6a40dd08a6888761327abd4e9555dcf353'
 monero_core_custom_url='https://github.com/mymonero/monero-core-custom'
-monero_core_custom_hash='5fe78a49307968f756d7cc43ab364c96f2f6f0b0'
+monero_core_custom_hash='d0d4ec2c99a1db96518a98ff47773d76cf729d84'
 
 if [ "$(basename "$(pwd)")" != "mymonero-monero-client" ]; then
   echo "Should be ran from the repo dir!"
