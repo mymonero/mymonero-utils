8317665ae6faf8ee99fa88857fdd0e894fa9d5f5
Karl Buys <kbbuys@gmail.com>
2024-06-27 10:08:47 +0200

    Revert "build: removes capacitor-file-picker due to new dependencies not following semver"
    This reverts commit a7199373d4f221a919de00274849380b33d082f6.

---
+++ packages/capacitor-file-picker/dist/esm/definitions.js
@@ -0,0 +1,5 @@
+export class FiletTypes {
+}
+FiletTypes.IMAGE = "image"; // For any type of image file
+FiletTypes.PDF = "pdf"; // For .pdf files
+//# sourceMappingURL=definitions.js.map
---
+++ packages/capacitor-file-picker/dist/esm/index.d.ts
@@ -0,0 +1,4 @@
+import type { FilePickerPlugin } from "./definitions";
+declare const FilePicker: FilePickerPlugin;
+export * from "./definitions";
+export { FilePicker };
